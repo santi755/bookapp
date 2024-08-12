7057b65612c77b5ebab46aa67bba73a83b75dd00
Santi Mansilla <santi755@gmail.com>
2024-08-12 16:56:54 +0200

    Refactor import paths in front components

--- front/app/(tabs)/_layout.tsx
+++ front/app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/front/hooks/useColorScheme";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
