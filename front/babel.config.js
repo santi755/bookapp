5eb0f97cf4040d58b6774b45632ec4056a69a49f
Santiago <santi755@gmail.com>
2024-09-15 13:47:00 +0200

    Inversify config

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,13 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    presets: [['babel-preset-expo'], ["@babel/typescript"]],
     plugins: [
-      ['@babel/plugin-transform-flow-strip-types'],
-      ['@babel/plugin-proposal-decorators', {legacy: true}],
-      ['@babel/plugin-transform-class-static-block'],
-      ['@babel/plugin-transform-class-properties', {loose: true}],
-      // ...other plugins
-    ],
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      "@babel/plugin-transform-flow-strip-types",
+      ["@babel/plugin-proposal-class-properties", { "loose": true }]
+    ]
   };
 };
--- front/core/config/README.md
+++ front/core/config/README.md
@@ -1,1 +1,4 @@
 We are following this guide: https://thevinaysingh.medium.com/complete-configuration-of-inversify-and-plugins-in-react-native-13061f2b6bf6
+
+This repo really helped me understand how to use InversifyJS with React Native:
+https://github.com/nsainaney/inversifyBabel/tree/master
