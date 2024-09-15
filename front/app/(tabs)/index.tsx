231dd49780b92c0c80d56a62278f96b5212e3db4
Santiago <santi755@gmail.com>
2024-09-15 16:55:05 +0200

    Reset components

--- front/app/(tabs)/index.tsx
+++ front/app/(tabs)/index.tsx
@@ -1,38 +1,12 @@
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text } from "react-native";
 import { styled } from "nativewind";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 
 const StyledText = styled(Text);
 
 export default function HomeScreen() {
   return (
     <View>
-      <StyledText className="text-red-600">
-        This app includes example code to help you get started. 2
-      </StyledText>
+      <StyledText className="text-red-600">Index page!</StyledText>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
