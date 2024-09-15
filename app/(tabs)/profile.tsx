a86f42c8f05bf301d6146ab0feb45698b1f1edde
Santi Mansilla <santi755@gmail.com>
2024-09-15 20:05:10 +0200

    Refactor import statements and update i18n usage in HomeScreen component

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,16 @@
-import { View } from "react-native";
+import { SafeAreaView, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import { Paragraph } from "@/components/atoms/text/paragraph";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Paragraph className="text-blue-500">profile page...</Paragraph>
-      <Paragraph className="text-blue-500">Otro text 2o!...</Paragraph>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="auto" />
+      <View>
+        <Paragraph className="text-blue-500">profile page 2...</Paragraph>
+        <Paragraph className="text-blue-500">Otro text 2o!...</Paragraph>
+      </View>
+    </SafeAreaView>
   );
 }
